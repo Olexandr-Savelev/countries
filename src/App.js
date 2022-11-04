f489c8e302637645711fb99dc8cc5e3c36f0bd1f
Olexandr-Savelev <a.savelev.krm@gmail.com>
2022-11-04 15:49:41 +0200

    change title of application

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from "react-router-dom"
+
+import Header from "./components/Header"
+import Main from "./components/Main"
+import { HomePage } from "./routes/HomePage"
+import Details from "./routes/Details"
+import PageNotFound from "./routes/PageNotFound"
+import { useState } from "react"
 
 function App() {
+  const [countries, setCountries] = useState([])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Header />
+      <Main>
+        <Routes>
+          <Route
+            index
+            element={
+              <HomePage countries={countries} setCountries={setCountries} />
+            }
+          />
+          <Route path="*" element={<PageNotFound />} />
+          <Route path="/country/:name" element={<Details />} />
+        </Routes>
+      </Main>
+    </>
+  )
 }
 
-export default App;
+export default App
